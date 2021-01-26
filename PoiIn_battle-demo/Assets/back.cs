c959b0f2cef5b03cc25117d55364c0c4fdd41b1b
Takanashi-Rika <2824538599@qq.com>
2021-01-26 23:30:12 +0800

    耀君 添加了 UI环境

---
+++ PoiIn_battle-demo/Assets/back.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class back : MonoBehaviour
+{
+    public GameObject[] board;
+    public bool active1;
+    public bool active2;
+    private void Start()
+    {
+        board = GameObject.FindGameObjectsWithTag("board");
+        board[0].transform.position = new Vector3(0, 1, -4);
+        board[0].SetActive(false);
+        active1 = false;
+        board[1].transform.position = new Vector3(0, 1, -4);
+        board[1].SetActive(false);
+        active2 = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown (KeyCode.Tab))
+        {
+            if (active1 == true)
+            {
+                board[0].SetActive(false);
+                active1 = false;
+            }
+            else
+            {
+                board[0].SetActive(true);
+                active1 = true;
+            }
+        } 
+        if (Input.GetKeyDown (KeyCode.LeftShift))
+        {
+            if (active2 == true)
+            {
+                board[1].SetActive(false);
+                active2 = false;
+            }
+            else
+            {
+                board[1].SetActive(true);
+                active2 = true;
+            }
+        } 
+    }
+}
---
+++ PoiIn_battle-demo/Assets/backbag/Materials/TextureChange.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+public class TextureChange : AssetPostprocessor
+{
+    private void OnPreprocessTexture() {
+        //引入assetImporter(所有导入器的父类)转换为TextureImporter导入器
+        TextureImporter importer = (TextureImporter)assetImporter;
+        //将所有导入图片设置成 Spritre 类型
+        importer.textureType = TextureImporterType.Sprite;
+    }
+}
